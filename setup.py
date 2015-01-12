90f42fb09993676cede5ded17fa23c9e029ac70f
Oliver Bestwalter <oliver@bestwalter.de>
2015-01-12 22:10:25 +0100

    meta data and long description from README.rst

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ class PyenvInstall(install):
     def run(self):
         print(subprocess.check_output(['bash', 'bin/pyenv-installer']))
 
+with open('README.rst') as file:
+    long_description = file.read()
 
 distutils.core.setup(
-    name='pyenv',
     version='20150113',
+    name='pyenv',
+    author='Yamashita, Yuu',
+    url='https://github.com/yyuu/pyenv-installer',
     description='Installs pyenv and friends',
+    long_description=long_description,
+    license='MIT',
+    platforms=['UNIX'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
