608b750ce72b0aaabd69c0b35124a94a29edd252
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-09-30 16:21:55 +0200

    #48 Make the message more helpful and visible

--- pypi-dummy/setup.py
+++ pypi-dummy/setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 from setuptools.command.install import install
 
 project_url = 'https://github.com/yyuu/pyenv-installer'
+install_command = 'curl -L https://raw.githubusercontent.com/pyenv/' \
+                  'pyenv-installer/master/bin/pyenv-installer | bash'
 
 with open('pypi-readme.rst') as file:
     long_description = file.read()
@@ -10,8 +12,13 @@ with open('pypi-readme.rst') as file:
 
 class NoInstall(install):
     def run(self):
-        sys.exit("NOTE: this package is not installable with pip."
-                 "Please follow instructions on %s" % project_url)
+        sys.exit(
+            "################ NOTE ################\n"
+            "This package is not installable with pip. Install it with:\n\n"
+            "%s\n\n"
+            "See full instructions at %s\n" 
+            "######################################\n" % (
+                install_command, project_url))
 
 setup(
     version='0.0.0',
