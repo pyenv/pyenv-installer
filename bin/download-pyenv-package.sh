9629142bd8cd579ba299ce5ef3c685affbab9037
George Pickering <9803299+bigpick@users.noreply.github.com>
2022-11-30 19:26:08 -0500

    fix: Offline installer invalid git URLs

--- bin/download-pyenv-package.sh
+++ bin/download-pyenv-package.sh
@@ -11,18 +11,18 @@ fi
 TMP_DIR=$(mktemp -d)
 
 if [ -n "${USE_HTTPS}" ]; then
-  GITHUB="https://github.com"
+  GITHUB="https://github.com/"
 else
-  GITHUB="git://github.com"
+  GITHUB="git@github.com:"
 fi
 
 # checkout to temporary directory.
-checkout "${GITHUB}/pyenv/pyenv.git"            "$TMP_DIR"
-checkout "${GITHUB}/pyenv/pyenv-doctor.git"     "$TMP_DIR"
-checkout "${GITHUB}/pyenv/pyenv-installer.git"  "$TMP_DIR"
-checkout "${GITHUB}/pyenv/pyenv-update.git"     "$TMP_DIR"
-checkout "${GITHUB}/pyenv/pyenv-virtualenv.git" "$TMP_DIR"
-checkout "${GITHUB}/pyenv/pyenv-which-ext.git"  "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv.git"            "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv-doctor.git"     "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv-installer.git"  "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv-update.git"     "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv-virtualenv.git" "$TMP_DIR"
+checkout "${GITHUB}pyenv/pyenv-which-ext.git"  "$TMP_DIR"
 
 # create archive.
 tar -zcf "$PYENV_PACKAGE_ARCHIVE" -C "$TMP_DIR" .
